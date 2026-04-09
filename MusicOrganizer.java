6d725f05027d35dd7af39273b126cc7284992434
Minh Nguyen <allvernguyen@gmail.com>
2026-04-09 01:17:25 -0400

    Update MusicOrganizer.java

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -1,36 +1,106 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
+import java.util.Collections;
 
-public class MusicOrganizer {
-    private ArrayList<String> tracks;
-    private Random random;
-
-    public MusicOrganizer() {
-        tracks = new ArrayList<>();
-        random = new Random();
+public class MusicOrganizer
+{
+    private ArrayList<String> files;
+        
+    public MusicOrganizer()
+    {
+        files = new ArrayList<>();
+        
     }
-
-    public void addTrack(String track) {
-        tracks.add(track);
+    
+    public void addFile(String filename)
+    {
+        files.add(filename);
     }
-
-    public void playRandomTrack() {
-        if (tracks.size() == 0) {
-            System.out.println("No tracks available");
+    
+    public int getNumberOfFiles()
+    {
+        return files.size();
+    }
+    
+    public void listFile(int index) {
+        if (validIndex(index)) {
+        String filename = files.get(index);
+        System.out.println(filename);
+        } else {
+        System.out.println("Invalid index! Please use a value in the range 0-" + (files.size() - 1));
+        }
+    }
+    
+    public void removeFile(int index) {
+        if (validIndex(index) == true) {
+        files.remove(index);
+        System.out.println("File removed successfully.");
+        } else {
+        System.out.println("Invalid index! Please use a value in the range 0-" + (files.size() - 1));
+        }
+    }
+    
+    public void checkIndex(int index)
+    {
+        if (files.size() == 0){
+            System.out.println("YOU HAVE NO FILE, please add some files");
+        }
+        else if (index >= 0 && index <= files.size()-1){
+            System.out.println("VALID");
+        }
+        else{
+            System.out.println("NOT VALID, please use a value in the range 0-" + (files.size()-1));
+        }
+    }
+    
+    public boolean validIndex(int index) {
+        return index >= 0 && index < files.size();
+    }
+    
+    public void listAllFiles(){
+        int position = 0;
+        for(String filename : files){
+            System.out.println(position + ": " + filename);
+            position++;
+        }
+    }  
+    
+    public void listingMatching(String searchString){
+        boolean found = false;
+        for (String filename: files){
+            if(filename.contains(searchString)){
+                System.out.println(filename);
+                found = true;
+            }
+        }
+        if(!found){
+                System.out.println("the file is not founded");
+        }
+    }
+    
+    public void playARandomTrack()
+    {
+        if(files.size() == 0){
+            System.out.println("There is no files have been founded, please add a files!");
             return;
         }
-
-        int index = random.nextInt(tracks.size());
-        System.out.println("Playing: " + tracks.get(index));
+        Random rand = new Random();
+        int index = rand.nextInt(files.size());
+        
+        String filename = files.get(index);
+        System.out.println("Now playing: " + filename);
     }
-
-    public void playAllRandom() {
-        ArrayList<String> copy = new ArrayList<>(tracks);
-        Collections.shuffle(copy);
-
-        for (String track : copy) {
-            System.out.println("Playing: " + track);
+    
+    public void playAllRandomOrder()
+    {
+        if (files.size() == 0){
+            System.out.println("There is no files have been founded, please add a files!");
+            return;
+        }
+        ArrayList<String> shuffledFiles = new ArrayList<>(files);
+        Collections.shuffle(shuffledFiles);
+        for (String filename : shuffledFiles){
+            System.out.println("Now playing: " + filename);
         }
     }
 }
